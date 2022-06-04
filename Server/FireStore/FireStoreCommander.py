95fb3b290517ebbb50b5a518f4525d406c00c476
Minkai Chen <t110318092@ntut.edu.tw>
2022-06-04 23:03:47 +0800

    #Server: update comment and fix get_collection_last_idx method

--- Server/FireStore/FireStoreCommander.py
+++ Server/FireStore/FireStoreCommander.py
@@ -6,19 +6,32 @@ class FireStoreCommander():
     def __init__(self):
         self.client = firestore.client()
     
-    #取得目前集合最新的資料id
+    
+    # Get last id of a collection
     def get_collection_last_idx(self,collection_name):
-        current_size =0
+        last_idx =0
         try:
-          collection = self.client.collection(collection_name).get()
-          count = len(collection)
-          current_size = int(collection[count - 1].id)
+          
+          query_data = {}
+          datas = self.client.collection(collection_name).get()
+          for data in datas:
+             query_data[data.id]=data.to_dict() 
+             
+          last_idx = sorted([int(x) for x in query_data.keys()])[-1]
+
         except Exception as e:
           print('{}'.format(e))
         finally:
-          return current_size
+          return last_idx
 
-    #查詢資料
+    # Query data in Collection
+    #  Method:
+    #   Query all data in         : query(collection_name = COLLECTION)
+    #   Query by document id      : query(collection_name = COLLECTION , id = ID)
+    #   Query by column condition : query(collection_name = COLLECTION , column_name = COLUMN_NAME , key_word = KEY_WORD)
+    #  (where column_name=keyword)
+    
+    
     def query(self,collection_name, id = None, column_name = None , key_word = None):
         query_data={}
         is_success = True
@@ -47,7 +60,8 @@ class FireStoreCommander():
         finally:
           return self.return_data_processor(is_success,error_message,query_data)
       
-    #插入資料  
+      
+    # Insert data into collection  
     def insert(self,collection_name,data):
         id = self.get_collection_last_idx(collection_name) + 1
         current_document = self.client.collection(collection_name).document(str(id))
@@ -61,7 +75,8 @@ class FireStoreCommander():
         finally:
           return self.return_data_processor(is_success,error_message,'')
       
-    #更新資料
+      
+    # Update data by document id in a collection
     def update(self,collection_name,id,data):
         current_document = self.client.collection(collection_name).document(str(id))
         is_success = True
@@ -74,7 +89,8 @@ class FireStoreCommander():
         finally:
           return self.return_data_processor(is_success,error_message,'')
     
-    #刪除資料 
+    
+    # Delete data by document id in a collection
     def delete(self,collection_name,id):
         current_document = self.client.collection(collection_name).document(str(id))
         is_success = True
@@ -87,9 +103,10 @@ class FireStoreCommander():
         finally:
           return self.return_data_processor(is_success,error_message,'')
       
-    #整理成統一回傳格式
+      
+    # Pack data into a specific format
     def return_data_processor(self,is_success,message,data):
-        return { 'is_success' : is_success , 'message' : message , 'data' : data }
+        return {'is_success' : is_success , 'message' : message , 'data' : data }
       
 
      
