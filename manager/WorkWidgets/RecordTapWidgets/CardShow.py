cfdd8ccf9622071ee50f90cca45e756c783a28fe
heryiyu <a0970927705@gmail.com>
2022-06-07 15:49:53 +0800

    fourth_manager

--- manager/WorkWidgets/RecordTapWidgets/CardShow.py
+++ manager/WorkWidgets/RecordTapWidgets/CardShow.py
@@ -1,13 +1,14 @@
 from PyQt5 import QtWidgets,QtCore,QtGui
 from WorkWidgets.WidgetComponents import LabelComponent, LineEditComponent, ButtonComponent
 from WorkWidgets.SocketClient.ClientControl import ExecuteCommand
+import datetime
 import json
 
 class CardShow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setObjectName("show_stu_widget")
-        self.calendar = ShortDatetimeEdit()
+        self.calendar = ShortDatetimeEdit(self.custom_followUP)
         layout = QtWidgets.QVBoxLayout()
         functiom_layout = QtWidgets.QHBoxLayout()
         one_day_button = ButtonComponent("past day")
@@ -70,10 +71,11 @@ class CardShow(QtWidgets.QWidget):
     
     def custom_action(self):
         self.calendar.showcalendar()
-        now = QtCore.QDateTime.currentDateTime()
-        current_datetime = now.toString("yyyy/MM/dd hh:mm:ss")
-        past_datetime = now.addDays(-7).toString("yyyy/MM/dd hh:mm:ss")
-        self.show_logs(past_datetime,current_datetime)
+        
+    
+    def custom_followUP(self,stare_time,end_time):
+        self.show_logs(stare_time,end_time)
+        
 
 class showtable(QtWidgets.QTableWidget):
     def __init__(self):
@@ -92,37 +94,110 @@ class showtable(QtWidgets.QTableWidget):
         self.setRowCount(0)
         
 class ShortDatetimeEdit(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self,call_back_time):
         super(ShortDatetimeEdit, self).__init__()
-        
-        calendar = QtWidgets.QCalendarWidget(self)
-        calendar.setGridVisible(True)
-        calendar.move(20, 20)
-        calendar.clicked[QtCore.QDate].connect(self.showDate)
-
-        start_label = LabelComponent(12,"start time:")
-        end_label = LabelComponent(12,"end time:")
-        get_start_label = LabelComponent(12,"")
-        get_end_label = LabelComponent(12,"")
-        start_confirm_button = ButtonComponent("confirm")
-        end_confirm_button = ButtonComponent("confirm")
-        contents_layout = QtWidgets.QGridLayout()
-        contents_layout.addWidget(start_label,0,0,1,1)
-        contents_layout.addWidget(end_label,1,0,1,1)
-        contents_layout.addWidget(get_start_label,0,1,1,1)
-        contents_layout.addWidget(get_end_label,1,1,1,1)
-        contents_layout.addWidget(start_confirm_button,0,2,1,1)
-        contents_layout.addWidget(end_confirm_button,1,2,1,1)
-        contents_layout.setColumnStretch(0,1)
-        contents_layout.setColumnStretch(0,3)
-        contents_layout.setColumnStretch(0,1)
-        calendar.layout().addChildLayout(contents_layout)
-
-        self.setGeometry(100,100,300,300)
+        self.StartClicked = True
+        self.EndClicked = False
+        self.date =""
+        self.call_back_time = call_back_time
         self.setWindowTitle('Calendar')
         
+        low_date = QtCore.QDate(2022, 1, 1)
+        up_date = QtCore.QDate.currentDate()
+        calendar = QtWidgets.QCalendarWidget(self)
+        calendar.setDateRange(low_date, up_date)
+        calendar.setGridVisible(True)
+        calendar.move(20, 20)
+        calendar.clicked[QtCore.QDate].connect(self.GetDate)        
+
+        start_label = LabelComponent(10,"start time:")
+        end_label = LabelComponent(10,"end time:")
+        self.get_start_lineEdit = LineEditComponent("",font_size=10)
+        self.get_end_lineEdit = LineEditComponent("",font_size=10)
+        self.get_start_lineEdit.mousePressEvent = self.GetStartDate
+        self.get_end_lineEdit.mousePressEvent = self.GetEndDate
+        self.start_time_edit = QtWidgets.QTimeEdit()
+        self.end_time_edit = QtWidgets.QTimeEdit()
+        self.start_time_edit.setDisplayFormat("HH:mm:ss")
+        self.end_time_edit.setDisplayFormat("HH:mm:ss")
+        self.start_time_edit.setTime(QtCore.QTime.currentTime())
+        self.end_time_edit.setTime(QtCore.QTime.currentTime())
+        confirm_button = ButtonComponent("confirm",font_size=8)
+        confirm_button.clicked.connect(self.DateProcessing)
+        self.show_label = LabelComponent(12,"")
+        
+        start_label.setStyleSheet("color: black")
+        end_label.setStyleSheet("color: black")
+        self.show_label.setStyleSheet("color: red")
+        self.get_start_lineEdit.setStyleSheet("color: black")
+        self.get_end_lineEdit.setStyleSheet("color: black")
+        confirm_button.setStyleSheet("color: black")
+        
+        layout = QtWidgets.QVBoxLayout()
+        contents_layout = QtWidgets.QGridLayout()
+        confirm_layout = QtWidgets.QHBoxLayout()
+        
+        contents_layout.addWidget(start_label,0,0,1,1)
+        contents_layout.addWidget(end_label,1,0,1,1)
+        contents_layout.addWidget(self.get_start_lineEdit,0,1,1,1)
+        contents_layout.addWidget(self.get_end_lineEdit,1,1,1,1)
+        contents_layout.addWidget(self.start_time_edit,0,2,1,1)
+        contents_layout.addWidget(self.end_time_edit,1,2,1,1)
+        
+        confirm_layout.addWidget(self.show_label,9)
+        confirm_layout.addWidget(confirm_button,1)
+        
+        layout.addWidget(calendar)
+        layout.addLayout(contents_layout)
+        layout.addLayout(confirm_layout)
+        self.setLayout(layout)
+        
     def showcalendar(self):
+        currentDate = QtCore.QDate.currentDate().toString("yyyy/MM/dd")
+        self.get_start_lineEdit.setText("")
+        self.get_end_lineEdit.setText(currentDate)
+        self.end_time_edit.setTime(QtCore.QTime.currentTime())
+        
         self.show()
     
-    def showDate(self, date):
-        self.date = date.toString()       
+    def GetDate(self, date):
+        print(date)
+        self.date = date.toString("yyyy/MM/dd")
+        if self.StartClicked:
+            self.get_start_lineEdit.setText(self.date)
+        else:
+            self.get_end_lineEdit.setText(self.date)
+    
+    def GetStartDate(self, event):
+        self.StartClicked = True
+        
+    def GetEndDate(self, event):
+        self.StartClicked = False
+    
+    def DateProcessing(self):
+        warning = ""
+        start_date = self.get_start_lineEdit.text()
+        end_date = self.get_end_lineEdit.text()
+        print(start_date,end_date)
+        if not start_date:
+            warning = "start_time didn't select"
+        if not end_date:
+            warning = "end_time didn't select"
+        if start_date and end_date:
+            start_time = self.start_time_edit.time().toString("hh:mm:ss")
+            end_time = self.end_time_edit.time().toString("hh:mm:ss")
+            start_datetime = start_date+" "+start_time
+            end_datetime = end_date+" "+end_time
+            date_start = datetime.datetime.strptime(start_datetime, '%Y/%m/%d %H:%M:%S')
+            date_end = datetime.datetime.strptime(end_datetime, '%Y/%m/%d %H:%M:%S')
+            if date_start<date_end:
+                self.SentDate(start_datetime,end_datetime)
+            else:
+                warning = "The start time must be earlier than the end time"
+        self.show_label.setText(warning)
+            
+            
+        
+    def SentDate(self,StartTime,EndTime):
+        self.call_back_time(StartTime,EndTime)
+        self.hide()
