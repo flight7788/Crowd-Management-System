68e392916a09c02804e006dbf8f4f8367c9c6b5c
heryiyu <a0970927705@gmail.com>
2022-06-07 17:04:52 +0800

    modify StuAdd typesetting

--- manager/WorkWidgets/ManageWidgets/StuAdd.py
+++ manager/WorkWidgets/ManageWidgets/StuAdd.py
@@ -7,8 +7,7 @@ import json
 class StuAdd(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        layout = QtWidgets.QGridLayout()
-        
+        self.show_label = LabelComponent(16,"")
         name_label = LabelComponent(16,"姓名")
         self.name_input = LineEditComponent("")
         stuid_label = LabelComponent(16,"學號")
@@ -18,24 +17,26 @@ class StuAdd(QtWidgets.QWidget):
         self.add_botton = ButtonComponent("新增")
         self.add_botton.clicked.connect(lambda: self.add())
         
-        layout.addWidget(name_label, 0,1,1,1)
-        layout.addWidget(self.name_input, 0,2,1,2)
-        layout.addWidget(stuid_label, 1,1,1,1)
-        layout.addWidget(self.stuid_input, 1,2,1,2)
-        layout.addWidget(cardid_label, 2,1,1,1)
-        layout.addWidget(self.cardid_input, 2,2,1,2)
-        layout.addWidget(self.add_botton, 3,4,1,1)
-        layout.setColumnStretch(0, 3)
-        layout.setColumnStretch(1, 1)
-        layout.setColumnStretch(2, 1)
-        layout.setColumnStretch(3, 1)
-        layout.setColumnStretch(4, 1)
-        layout.setColumnStretch(5, 3)
-        layout.setRowStretch(0, 2)
-        layout.setRowStretch(1, 2)
-        layout.setRowStretch(2, 2)
-        layout.setRowStretch(3, 2)
-        layout.setRowStretch(4, 2)
+        
+        layout = QtWidgets.QHBoxLayout()
+        work_layout = QtWidgets.QGridLayout()
+        work_layout.addWidget(name_label, 0,0,1,1)
+        work_layout.addWidget(self.name_input, 0,1,1,1)
+        work_layout.addWidget(stuid_label, 1,0,1,1)
+        work_layout.addWidget(self.stuid_input, 1,1,1,1)
+        work_layout.addWidget(cardid_label, 2,0,1,1)
+        work_layout.addWidget(self.cardid_input, 2,1,1,1)
+        work_layout.addWidget(self.add_botton, 3,1,1,1)
+        work_layout.setColumnStretch(0, 1)
+        work_layout.setColumnStretch(1, 2)
+        work_layout.setRowStretch(0, 2)
+        work_layout.setRowStretch(1, 2)
+        work_layout.setRowStretch(2, 2)
+        work_layout.setRowStretch(3, 2)
+        work_layout.setRowStretch(4, 2)
+        
+        layout.addLayout(work_layout,4)
+        layout.addWidget(self.show_label,6)
         self.setLayout(layout)
         
     def load(self):
@@ -44,17 +45,26 @@ class StuAdd(QtWidgets.QWidget):
         self.cardid_input.setText("")
     
     def add(self):
-        if (self.name_input.text() and self.stuid_input.text() and self.cardid_input.text()):
-            stu_info = {'student_id': self.stuid_input.text(),
-                        'card_no': self.cardid_input.text(),
-                        'student_name': self.name_input.text()}
+        student_id= self.stuid_input.text()
+        card_no = self.cardid_input.text()
+        student_name = self.name_input.text()
+        if (student_id and card_no and student_name):
+            stu_info = {'student_id': student_id,
+                        'card_no': card_no,
+                        'student_name': student_name}
             self.execute_query = ExecuteCommand(command='add_stu',data=stu_info)
             self.execute_query.start()
             self.execute_query.return_sig.connect(self.add_followUp)
+        else:
+            warning = "There are fields not entered"
+            self.show_label.setText(warning)
     
     def add_followUp(self,response):
         response = json.loads(response)
         if response['status'] == 'OK':
+            warning = "Add Success"
+            self.show_label.setText(warning)
             self.load()
         else:
-            pass
+            warning = "Add Fail\n"+response["reason"]
+        self.show_label.setText(warning)
