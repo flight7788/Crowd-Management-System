a474e395976f2c9acb4cda24209e503fd1e2c366
heryiyu <a0970927705@gmail.com>
2022-06-09 03:39:28 +0800

    renew CardAnalysis plot

--- manager/WorkWidgets/RecordTapWidgets/CardAnalyz.py
+++ manager/WorkWidgets/RecordTapWidgets/CardAnalyz.py
@@ -24,7 +24,7 @@ class CardAnalyz(QtWidgets.QWidget):
     
     def QueryData(self):
         now = QtCore.QDateTime.currentDateTime()
-        current_datetime = now.toString("yyyy/MM/dd hh:mm:ss")
+        current_datetime = now.toString("yyyy/MM/07 hh:mm:ss")
         past_datetime = now.addDays(-7).toString("yyyy/MM/dd hh:mm:ss")
         date = {'start_time':past_datetime, 'end_time':current_datetime}
         self.execute_query = ExecuteCommand(command='query_logs',data=date)
@@ -32,17 +32,23 @@ class CardAnalyz(QtWidgets.QWidget):
         self.execute_query.return_sig.connect(self.get_info_follwUp)
     
     def get_info_follwUp(self,response):
-        population_y = [10, 30, 20]
-        date_name = ["A","B","C"]
+        population_y = list()
+        date_name = list()
+        repeat_date = list()
+        repeat_y = list()
         response = json.loads(response)
         if response['status'] == 'OK':
             stu_list = list()
             timedict = dict()
-            population_y = list()
-            date_name = list()
+            repeat_timedict = dict()
             record_list = response['data']
             for student in record_list:
                 if student['status'] == 'in':
+                    key = student['swipe_time'][5:10]
+                    if key in timedict.keys():
+                        repeat_timedict[key]+=1
+                    else:
+                        repeat_timedict[key] = 1
                     if not student['card_no'] in stu_list:
                         stu_list.append(student['card_no'])
                         key = student['swipe_time'][5:10]
@@ -53,12 +59,19 @@ class CardAnalyz(QtWidgets.QWidget):
             for k, v in timedict.items():
                 date_name.append(k)
                 population_y.append(v)
-        
+            for k, v in repeat_timedict.items():
+                repeat_date.append(k)
+                repeat_y.append(v)
+        self.draw_plot(date_name,population_y,repeat_date,repeat_y)
+                
+    def draw_plot(self,date_name,population_y,repeat_date,repeat_y):
         index_x = list(range(1,len(date_name)+1))
-        barItem = pg.BarGraphItem(x = index_x, height = population_y, width = 0.3, brush=(107,200,224))
-        self.plt.addItem(barItem)
-        
-        self.plt.getAxis('bottom').setTicks([[(i, date_name[i-1]) for i in index_x]])
+        re_index_x = list(range(1,len(repeat_date)+1))
+        no_barItem = pg.BarGraphItem(x = re_index_x, height = population_y, width = 0.2, brush=(107,200,224))
+        repeat_barItem = pg.BarGraphItem(x = index_x, y0=population_y,height = repeat_y, width = 0.2, brush=(224,200,107))
+        self.plt.addItem(no_barItem)
+        self.plt.addItem(repeat_barItem)
+        self.plt.getAxis('bottom').setTicks([[(i, repeat_date[i-1]) for i in re_index_x]])
         font = QtGui.QFont()
         font.setPixelSize(20)
         self.plt.getAxis("bottom").setStyle(tickFont = font)
